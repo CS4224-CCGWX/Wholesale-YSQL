f99a4ad177c16fbb8aa65d573714f4ff4b2f2245
Yaoren Xie <xie.yaoren@bytedance.com>
2022-10-31 22:12:31 +0800

    change in dump data

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -8,15 +8,17 @@ YSQLSH="/temp/yugabyte-2.14.1.0/bin/ysqlsh"
 dataDir="/home/stuproj/cs4224i/Wholesale-YSQL/project_files/data_files"
 bsz=500
 
+
 echo "***** Remove null in dataset *****"
 python preprocess/precompute.py
 
 echo "***** Start dump data *****"
 echo "Defining schema"
-$YSQLSH -f $schema --request-timeout=3600
+$YSQLSH -f $schema
 
 
 curr_node=$1
+port=$2
 if [[ $curr_node == "xcnd20" ]]; then
     ip="192.168.48.239"
 elif [[ $curr_node == "xcnd21" ]]; then
@@ -34,22 +36,22 @@ fi
 
 # DateTime format reference: https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
 echo "***** Load warehouse table *****"
-$YSQLSH -c "\COPY warehouse FROM '$dataDir/warehouse.csv' DELIMITER '$DELIM';"
+$YSQLSH  -c "\COPY warehouse FROM '$dataDir/warehouse.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load district table *****"
-$YSQLSH -c "\COPY district FROM '$dataDir/district.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY district FROM '$dataDir/district.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load customer table *****"
-$YSQLSH -c "\COPY customer FROM '$dataDir/customer.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY customer FROM '$dataDir/customer.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load order table *****"
-$YSQLSH -c "\COPY \"order\" FROM '$dataDir/customer.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY \"order\" FROM '$dataDir/customer.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load item table *****"
-$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load order_line table *****"
-$YSQLSH -c "\COPY order_line FROM '$dataDir/order_line.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY order_line FROM '$dataDir/order_line.csv' DELIMITER '$DELIM';" -h $ip -p $port
 
 echo "***** Load stock table *****"
-$YSQLSH -c "\COPY stock FROM '$dataDir/stock.csv' DELIMITER '$DELIM';"
+$YSQLSH -c "\COPY stock FROM '$dataDir/stock.csv' DELIMITER '$DELIM';" -h $ip -p $port
