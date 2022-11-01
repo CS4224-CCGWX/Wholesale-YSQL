a2b9d6b8ff166d10c8973be0f6a6999f2e8234d9
Yaoren Xie <xie.yaoren@bytedance.com>
2022-11-01 12:37:49 +0800

    change

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -16,44 +16,41 @@ echo "Defining schema"
 $YSQLSH -f $schema
 
 
-#curr_node=$1
+curr_node=$1
 #port=$2
-#if [ $curr_node == "xcnd20" ]; then
-#    ip="192.168.48.239"
-#elif [ $curr_node == "xcnd21" ]; then
-#    ip="192.168.48.240"
-#elif [ $curr_node == "xcnd22" ]; then
-#    ip="192.168.48.241"
-#elif [ $curr_node == "xcnd23" ]; then
-#    ip="192.168.48.242"
-#elif [ $curr_node == "xcnd24" ]; then
-#    ip="192.168.48.243"
-#else
-#    echo "Using default node xcnd20"
-#    ip="192.168.48.239"
-#fi
-
-curr_node="192.168.48.239"
-port=5433
+if [ $curr_node == "xcnd20" ]; then
+    ip="192.168.48.239"
+elif [ $curr_node == "xcnd21" ]; then
+    ip="192.168.48.240"
+elif [ $curr_node == "xcnd22" ]; then
+    ip="192.168.48.241"
+elif [ $curr_node == "xcnd23" ]; then
+    ip="192.168.48.242"
+elif [ $curr_node == "xcnd24" ]; then
+    ip="192.168.48.243"
+else
+    echo "Using default node xcnd20"
+    ip="192.168.48.239"
+fi
 
 # DateTime format reference: https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
 echo "***** Load warehouse table *****"
 $YSQLSH  -c "\COPY warehouse FROM '$dataDir/warehouse.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
 
 echo "***** Load district table *****"
-$YSQLSH -c "\COPY district FROM '$dataDir/district.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY district FROM '$dataDir/district.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
 
 echo "***** Load customer table *****"
-$YSQLSH -c "\COPY customer FROM '$dataDir/customer.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY customer FROM '$dataDir/customer.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
 
 echo "***** Load order table *****"
-$YSQLSH -c "\COPY \"order\" FROM '$dataDir/order.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY \"order\" FROM '$dataDir/order.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
 
 echo "***** Load item table *****"
-$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
 
 echo "***** Load order_line table *****"
-$YSQLSH -c "\COPY order_line FROM '$dataDir/order_line.csv' WITH (FORMAT CSV, NULL '');" -h $ip -p $port
+$YSQLSH -c "\COPY order_line FROM '$dataDir/order_line.csv' WITH (FORMAT CSV, NULL '');" -h $ip
 
 echo "***** Load stock table *****"
-$YSQLSH -c "\COPY stock FROM '$dataDir/stock.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY stock FROM '$dataDir/stock.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip
