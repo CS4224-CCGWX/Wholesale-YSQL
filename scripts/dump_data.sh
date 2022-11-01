1037141614edffe74105653fea6cc58925da0a5c
Yaoren Xie <xie.yaoren@bytedance.com>
2022-11-01 12:33:46 +0800

    change scripts

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -16,22 +16,25 @@ echo "Defining schema"
 $YSQLSH -f $schema
 
 
-curr_node=$1
-port=$2
-if [ $curr_node == "xcnd20" ]; then
-    ip="192.168.48.239"
-elif [ $curr_node == "xcnd21" ]; then
-    ip="192.168.48.240"
-elif [ $curr_node == "xcnd22" ]; then
-    ip="192.168.48.241"
-elif [ $curr_node == "xcnd23" ]; then
-    ip="192.168.48.242"
-elif [ $curr_node == "xcnd24" ]; then
-    ip="192.168.48.243"
-else
-    echo "Using default node xcnd20"
-    ip="192.168.48.239"
-fi
+#curr_node=$1
+#port=$2
+#if [ $curr_node == "xcnd20" ]; then
+#    ip="192.168.48.239"
+#elif [ $curr_node == "xcnd21" ]; then
+#    ip="192.168.48.240"
+#elif [ $curr_node == "xcnd22" ]; then
+#    ip="192.168.48.241"
+#elif [ $curr_node == "xcnd23" ]; then
+#    ip="192.168.48.242"
+#elif [ $curr_node == "xcnd24" ]; then
+#    ip="192.168.48.243"
+#else
+#    echo "Using default node xcnd20"
+#    ip="192.168.48.239"
+#fi
+
+curr_node="192.168.48.239"
+port=5433
 
 # DateTime format reference: https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
 echo "***** Load warehouse table *****"
@@ -47,7 +50,7 @@ echo "***** Load order table *****"
 $YSQLSH -c "\COPY \"order\" FROM '$dataDir/order.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
 
 echo "***** Load item table *****"
-$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' DELIMITER WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
+$YSQLSH -c "\COPY item FROM '$dataDir/item.csv' WITH (FORMAT CSV, NULL 'null');" -h $ip -p $port
 
 echo "***** Load order_line table *****"
 $YSQLSH -c "\COPY order_line FROM '$dataDir/order_line.csv' WITH (FORMAT CSV, NULL '');" -h $ip -p $port
