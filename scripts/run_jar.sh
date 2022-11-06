2483f516fbc7e1975d02003951aac3f541ead644
Siyuan Cheng <siyuanc6794@gmail.com>
2022-11-06 20:41:58 +0800

    path

--- scripts/run_jar.sh
+++ scripts/run_jar.sh
@@ -3,16 +3,12 @@ tx=$3
 port=$2
 
 
-cd ..
-if [[ ! -e log ]]; then
-    mkdir log
-fi
 
 for ((c=0; c<4; c++))
 do
 	client=`expr $c \* 5  + $tx`
-	output_path=./log/$client.out
-	err_path=./log/$client.err
+	output_path=./$client.out
+	err_path=./$client.err
 	
 	echo "Run transaction file at node: $ip"
 	tmux new-session -d -s client$client "java -jar target/yugabyte-simple-java-app-1.0-SNAPSHOT.jar $ip $port $client run > $output_path 2>$err_path"
